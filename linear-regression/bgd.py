680ec96f32feb4ac0f7d45c9e7c3d8311d27cbe6
Daniel Armas <daniel62armas@gmail.com>
2024-03-19 23:48:38 -0600

    Complete BGD and SGD methods

--- linear-regression/bgd.py
+++ linear-regression/bgd.py
@@ -1,11 +1,37 @@
 from typing import List
 
 class BGD:
-    def __init__(self, x: List[List[int]], y: List[int], w: List[int])->None:
+    def __init__(self, x: List[List[int]], y: List[int], w: List[float], alpha: float)->None:
         self.x = x
         self.y = y
         self.w = w
+        self.alpha = alpha
 
-    def BGD(self, steps: int)->List[float]:
+    def BGD(self)->None:
+        n = len(self.y)
+
+        for i in range(n):
+
+            sum_vec = []
+            temp_array = []
+
+            for j in range(n):
+                temp_array.append((self.w[i] * self.x[i][j]) - self.y[j])
+
+            for j in range(n):
+                sum_vec.append(temp_array[j] * self.x[i][j])
+
+            all_sum = sum(sum_vec)
+            self.w[i] = self.w[i] - ((2*self.alpha) * all_sum)
+
+    def iteration(self, steps: int)->None:
         for i in range(steps):
-            pass
+            self.BGD()
+            print(self.w)
+
+x = [[1, 1, 1], [1, 2, 3], [1, 3, 5]]
+y = [5, 13, 21]
+w = [0, 0, 0]
+a = 0.01
+bgd = BGD(x, y, w, a)
+bgd.iteration(2)
---
+++ linear-regression/sgd.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class SGD:
+    def __init__(self, x: List[List[int]], y: List[int], w: List[float], alpha: float)->None:
+        self.x = x
+        self.y = y
+        self.w = w
+        self.alpha = alpha
+
+    def sgd(self, j) -> None:
+        n = len(self.w)
+        for i in range(n):
+            y = self.y[j]
+            xji = self.x[j][i]
+            self.w[i] = self.w[i] - ( ( 2 * self.alpha) * (( (self.w[i] * xji) - y) * xji) )
+
+    def iteration(self, steps, j) -> None:
+        for i in range(steps):
+            self.sgd(j)
+            print(self.w)
+
+x = [ [1, 1], [1, 2], [1, 3] ]
+y = [3, 6, 9]
+w = [0, 0]
+a = 0.01
+
+sGD = SGD(x, y, w, a)
+sGD.iteration(2, 2)
+
+
